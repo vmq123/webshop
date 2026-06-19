25d5e5ea5d44f8baf28f3167fc9b6c3adddc6563
vmq123 <vmq123@gmail.com>
2026-06-19 09:40:23 +0700

    u32

--- webshop/webshop/shopping_cart/product_info.py
+++ webshop/webshop/shopping_cart/product_info.py
@@ -8,7 +8,9 @@ from webshop.webshop.doctype.webshop_settings.webshop_settings import (
     show_quantity_in_website,
 )
 from webshop.webshop.shopping_cart.cart import _get_cart_quotation, _set_price_list
-from erpnext.utilities.product import (get_price)
+# from erpnext.utilities.product import (get_price)
+from touropt.controllers.item_pricing import get_price
+
 from webshop.webshop.utils.product import (get_non_stock_item_status, get_web_item_qty_in_stock)
 from webshop.webshop.shopping_cart.cart import get_party
 
