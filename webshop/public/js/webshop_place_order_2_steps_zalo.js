c2667f8c07fef309da3638b629eafb41d54ff0ff
vmq123 <vmq123@gmail.com>
2026-09-10 00:18:52 +0700

    u48

--- webshop/public/js/webshop_place_order_2_steps_zalo.js
+++ webshop/public/js/webshop_place_order_2_steps_zalo.js
@@ -22,7 +22,6 @@ function place_order_case_1_dialog(e){
 				label: __('Số tiền khách thanh toán'),
 				fieldname: 'amount_received',
 				fieldtype: 'Float',
-				options: 'VND',
 				default: amount
 			},
 			{
@@ -36,7 +35,6 @@ function place_order_case_1_dialog(e){
 				label: __(''),
 				fieldname: 'grand_total',
 				fieldtype: 'Float',
-				options: 'VND',
 				default: grand_total,
 				hidden: 1
 			},
